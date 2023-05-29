574d1acaa01a6d3be3dcb5937c9d129f6b54ff6f
nicoxd104 <nicolas.munoz.r@usach.cl>
2023-05-28 23:49:44 -0400

    Drive.pl
    Creaccion del addDrive

---
+++ Drive.pl
@@ -0,0 +1,38 @@
+%----------------------------- TDA drive ----------------------------------
+
+drive(Letra, Nombre, Capacidad, [Letra, Nombre, Capacidad]).
+
+addDrive_to_Drives(New_Drive, Drives, Updated_Drive):-
+    append(Drives, [New_Drive], Updated_Drive).
+
+set_Drives(Sistema, Updated_Drive, Updated_Sistema):-
+    filesystem(Nombre, _, Usuarios, Usuario, Ruta, Carpetas, Papelera, Sistema),
+    filesystem(Nombre, Updated_Drive, Usuarios, Usuario, Ruta, Carpetas, Papelera, Updated_Sistema).     
+
+get_drives(Sistema, Drives):-
+    nth0(1, Sistema, Drives).  
+
+get_letra(Elemento, Posicion):-
+    nth0(0, Elemento, Posicion).
+
+get_drives_2(Sistema, Drives_2):-
+    nth0(1, Sistema, Drives_2).
+
+verificar_letra(Letra, Elemento):-
+    get_letra(Elemento, Posicion),
+    member(Letra, Posicion).
+
+verificar_letra(Letra, [_|Resto]):-
+    verificar_letra(Letra, Resto).
+
+systemAddDrive(Sistema, Letra, Nombre, Capacidad, Updated_Sistema):-
+    get_drives(Sistema, Drives),
+    verificar_letra(Letra, Drives)-> 
+    get_drives(Sistema, Drives),
+    set_Drives(Sistema, Drives, Updated_Sistema)
+    ;
+    get_drives_2(Sistema, Drives_2),
+    drive(Letra, Nombre, Capacidad, New_Drive),
+    addDrive_to_Drives(New_Drive, Drives_2, Updated_Drive),
+    set_Drives(Sistema, Updated_Drive, Updated_Sistema).
+
