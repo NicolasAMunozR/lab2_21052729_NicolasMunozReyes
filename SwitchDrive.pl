667ea48b52f796b6a1f669cf342bbb4d1a497973
nicoxd104 <nicolas.munoz.r@usach.cl>
2023-06-05 21:32:49 -0400

    SwitchDrive.pl
    Modificacion de la funcion SwitchDrive con respecto a las cosas pedidas

--- SwitchDrive.pl
+++ SwitchDrive.pl
@@ -14,20 +14,11 @@ unir([Cadena|Cadenas], Resultado):-
     unir(Cadenas, Resto),
     atom_concat(Cadena, Resto, Resultado).
 
-confirmar(Sistema, Letra, Updated_Sistema):-
-    get_drives(Sistema, Drives),
-    verificar_letra(Letra, Drives)->
-    unir([Letra, ":/"], Letra_2),
-    ruta(Letra_2, Updated_Ruta),
-    set_ruta(Sistema, Updated_Ruta, Updated_Sistema)
-    ;
-    get_ruta(Sistema, Ruta),
-    set_ruta(Sistema, Ruta, Updated_Sistema).
-
 systemSwitchDrive(Sistema, Letra, Updated_Sistema):-
     get_usuario(Sistema, Usuario),
-    es_vacia_lista(Usuario)->
-    get_ruta(Sistema, Ruta),
-    set_ruta(Sistema, Ruta, Updated_Sistema)
-    ;
-    confirmar(Sistema, Letra, Updated_Sistema).
+    \+ es_vacia_lista(Usuario),
+    get_drives(Sistema, Drives),
+    verificar_letra(Letra, Drives),
+    unir([Letra, ":/"], Letra_2),
+    ruta(Letra_2, Updated_Ruta),
+    set_ruta(Sistema, Updated_Ruta, Updated_Sistema).
