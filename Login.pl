026b83cff7ed21b70a339f47dcb4723b8e8ed1f4
nicoxd104 <nicolas.munoz.r@usach.cl>
2023-06-05 21:31:54 -0400

    Login.pl
    Modificacion de la funcion Login con respecto a las cosas pedidas

--- Login.pl
+++ Login.pl
@@ -18,21 +18,18 @@ get_usuario_2(Sistema, Usuario_2):-
 es_vacia_lista(Usuario):-
     length(Usuario, 0).
 
-agregar(Sistema, User, Updated_Sistema):-
+systemLogin(Sistema, User, Updated_Sistema):-
+    get_usuario(Sistema, Usuario),
+    es_vacia_lista(Usuario),
     get_user(Sistema, Usuarios),
-    verificar_nombre(User, Usuarios)->
+    verificar_nombre(User, Usuarios),
     get_usuario_2(Sistema, Usuario_2),
     usuario(User, New_usuario),
     adduser_to_usuario(New_usuario, Usuario_2, Updated_Usuario),
-    set_usuario(Sistema, Updated_Usuario, Updated_Sistema)
-    ;
-    get_usuario(Sistema, Usuario),
-    set_usuario(Sistema, Usuario, Updated_Sistema).
-
-systemLogin(Sistema, User, Updated_Sistema):-
-    get_usuario(Sistema, Usuario),
-    es_vacia_lista(Usuario)-> 
-    agregar(Sistema, User, Updated_Sistema)
-    ;
-    get_usuario(Sistema, Usuario),
-    set_usuario(Sistema, Usuario, Updated_Sistema).
+    set_usuario(Sistema, Updated_Usuario, Updated_Sistema_1),
+    get_drives(Sistema, Drives),
+    primer_elemento(Drives, Drives_1),
+    primer_elemento(Drives_1, Drives_2),
+    unir([Drives_2, ":/"], Letra_2),
+    ruta(Letra_2, Updated_Ruta),
+    set_ruta(Updated_Sistema_1, Updated_Ruta, Updated_Sistema).
